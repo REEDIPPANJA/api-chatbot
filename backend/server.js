2bf2db769673161efd4a91d6cc5183df3ee9dcc3
REEDIPPANJA <panjareedip20@gmail.com>
2025-05-16 01:49:48 +0530

    final changes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,11 @@ const cors=require("cors");
 const axios = require("axios");
 const app = express();
 app.use(express.json());
-app.use(cors());   
+app.use(cors(
+  {
+    origin:"api-chatbot.netlify.app"
+  }
+));   
 
 app.post("/", async (req, res) => {
   try {

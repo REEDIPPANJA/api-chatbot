be1eb54b67dcd4d4e775ad6ec9238df55a2054cd
REEDIPPANJA <panjareedip20@gmail.com>
2025-05-15 19:26:58 +0530

    backend added

---
+++ script.js
@@ -0,0 +1,108 @@
+const btn=document.querySelector('#btn')
+const url= "http://localhost:3000/gemini"; 
+
+
+
+
+function addEle(ClassName,data){
+  const div=document.createElement('div');
+  div.className=ClassName;
+ 
+  const input=document.createElement('p');
+  input.id="userPrompt";
+  input.textContent=data;
+
+  const userImage = document.createElement('img');
+  userImage.src = 'user.png';
+  userImage.alt = '';
+  userImage.height = 50;
+
+div.appendChild(input);
+div.appendChild(userImage);
+
+document.querySelector('.chat-conatiner').appendChild(div);
+};
+
+
+
+
+function loading(){
+  const div=document.createElement('div');
+  div.className='ai-chatbox';
+
+const userImage = document.createElement('img');
+userImage.src = 'ai.png';
+userImage.alt = '';
+userImage.height = 50;
+
+const userImage2 = document.createElement('img');
+userImage2.src = 'load.gif';
+userImage2.alt = '';
+userImage2.height = 50;
+userImage2.className='loadingImg'
+
+
+div.appendChild(userImage);
+div.appendChild(userImage2);
+
+document.querySelector('.chat-conatiner').appendChild(div);
+}
+
+
+async function datafetching(userInput){
+    try{
+        let data=await fetch(url,{
+            method:'POST',
+            headers:{'Content-Type': 'application/json'},
+            body:JSON.stringify({
+                contents: [
+                    {"parts":[{text:userInput}]}
+                ]
+
+            })
+        });
+    let dataResponse=await data.json();
+   
+    const text=dataResponse?.candidates[0].content.parts[0].text;
+    console.log(text)
+    createrespose(text);
+    }
+    catch(error){
+        console.log(error);
+
+    }
+    
+}
+
+
+function createrespose(text){
+    const LastDiv=document.querySelector('.ai-chatbox:last-child');
+    const delChild=LastDiv.querySelector('.loadingImg');
+    LastDiv.removeChild(delChild);
+
+ const input=document.createElement('p');
+  input.id="aiPrompt";
+  input.textContent=text;
+
+  LastDiv.appendChild(input);
+
+}
+
+
+btn.addEventListener('click',()=>{
+    document.querySelector('.heading').style.display='none';
+    let userInput=document.querySelector('#input-box').value.trim();
+    if(!userInput){
+        alert("enter a valid prompt");
+        return;
+    }
+    console.log(userInput);
+    addEle('user-chatbox',userInput);
+    document.querySelector('#input-box').value="";
+    setTimeout(loading,300);
+   
+    datafetching(userInput)
+    
+
+
+})
---
+++ server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors=require("cors");
+const axios = require("axios");
+const app = express();
+app.use(express.json());
+app.use(cors());   
+
+app.post("/gemini", async (req, res) => {
+  try {
+    const response = await axios.post(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMENI_URL}`,
+      req.body
+    );
+    res.json(response.data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
